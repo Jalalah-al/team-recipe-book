3df1d09b837c74e4bfddbdae041cb7d585f4ab4b
Aufiyah <c5056894@hallam.shu.ac.uk>
2026-02-16 13:51:23 +0000

    Convert Flutter app from submodule to normal folder

---
+++ welcome_sheffield/lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'screens/home_screen.dart';
+
+void main() {
+  runApp(const WelcomeSheffieldApp());
+}
+
+class WelcomeSheffieldApp extends StatelessWidget {
+  const WelcomeSheffieldApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Welcome to Sheffield',
+      theme: ThemeData(
+        useMaterial3: true,
+        colorSchemeSeed: Colors.blue,
+      ),
+      home: const HomeScreen(),
+    );
+  }
+}

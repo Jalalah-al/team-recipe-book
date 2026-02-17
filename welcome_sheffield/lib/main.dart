5fa64ed84391dbddaf0ed0cd6e236f2255173e77
nrobleh <c4052172@hallam.shu.ac.uk>
2026-02-17 15:36:57 +0000

    filler content page

--- welcome_sheffield/lib/main.dart
+++ welcome_sheffield/lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'screens/content.dart';
+import 'package:google_fonts/google_fonts.dart';
+
 
 void main() {
   runApp(const WelcomeSheffieldApp());
@@ -12,12 +14,31 @@ class WelcomeSheffieldApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Welcome to Sheffield',
       theme: ThemeData(
         useMaterial3: true,
-        colorSchemeSeed: Colors.blue,
+
+        scaffoldBackgroundColor: Colors.white,
+
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF13384A),
+          foregroundColor: Colors.white,
+          centerTitle: false,
+        ),
+
+        bottomNavigationBarTheme: const BottomNavigationBarThemeData(
+          backgroundColor: Color(0xFF13384A),
+          selectedItemColor: Colors.white,
+          unselectedItemColor: Colors.white70,
+        ),
+
+        textTheme: GoogleFonts.poppinsTextTheme().apply(
+          bodyColor: Colors.black,
+          displayColor: Colors.black,
+        ),
       ),
-      home: const HomeScreen(),
+
+      home: const ContentPage(title: "Test Page"),
+
     );
   }
 }

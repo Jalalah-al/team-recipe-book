2b5d9c08ef2446d27b8b9dfc3fc896e4f52158bf
Aufiyah <c5056894@hallam.shu.ac.uk>
2026-02-18 10:03:17 +0000

    splash screen & select language screen

--- welcome_sheffield/lib/main.dart
+++ welcome_sheffield/lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
-import 'screens/home_screen.dart';
+import 'screens/splash_screen.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 void main() {
   runApp(const WelcomeSheffieldApp());
@@ -15,8 +16,18 @@ class WelcomeSheffieldApp extends StatelessWidget {
       title: 'Welcome to Sheffield',
       theme: ThemeData(
         useMaterial3: true,
-        colorSchemeSeed: Colors.blue,
+
+        scaffoldBackgroundColor: const Color(0xFF13384A),
+
+        appBarTheme: const AppBarTheme(
+          backgroundColor: Color(0xFF13384A),
+          foregroundColor: Colors.white,
+        ),
+
+        textTheme: GoogleFonts.poppinsTextTheme(),
       ),
+
+
       home: const HomeScreen(),
     );
   }
--- welcome_sheffield/lib/screens/splash_screen.dart
+++ welcome_sheffield/lib/screens/splash_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'language_screen.dart';
 
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
@@ -6,11 +7,54 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      backgroundColor: const Color(0xFF13384A),
       appBar: AppBar(
         title: const Text('Welcome to Sheffield'),
       ),
-      body: const Center(
-        child: Text('Home Screen Placeholder'),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Padding(
+              padding: const EdgeInsets.all(16),
+              child: Image.asset(
+                'lib/images/ICON.png',
+                height: 300,
+              ),
+            ),
+            const SizedBox(height: 40),
+            SizedBox(
+              width: 220,
+              child: ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: Colors.white,
+                  foregroundColor: const Color(0xFF13384A),
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 16,
+                  ),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(12),
+                  ),
+                ),
+                onPressed: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => const LanguageScreen(),
+                    ),
+                  );
+                },
+                child: const Text(
+                  'Choose Language',
+                  style: TextStyle(
+                    fontSize: 16,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
